fe1036f08c77c8f2e98533a2c6e9a20e71ac751d
agusescalante <agustinescalante27@gmail.com>
2022-11-04 13:45:39 -0300

    Principales atributos y constructor para la clase Persona

---
+++ functions/objectJava.java
@@ -0,0 +1,13 @@
+public class Persona {
+	private String nombre, apellido;
+	private LocalDate fec_nacimiento;
+
+	public Persona(String no, String ap, LocalDate nac){
+		nombre = no;
+		apellido = ap;
+		fec_nacimiento = nac;
+	}
+
+
+
+}
